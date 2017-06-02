010057400557220ce0d50d8b8d945550807e1270
rockawayz <rockawayz@hotmail.com>
2017-06-02 04:16:45 -0700

    Create css-method.js

---
+++ css-method.js
@@ -0,0 +1,11 @@
+Element.prototype.css = function() {
+		var elementStyle = this.style, args = arguments;
+		if (args.length == 2) {
+			return void(elementStyle.cssText += args[0] + ':' + args[1]);
+		} else if (args.length == 1) {
+			var cssString = JSON.stringify(args[0]),
+				computeStyle = JSON.parse(cssString, function(key, value) {
+					return key != "" ? elementStyle.cssText += key + ":" + value : elementStyle = ""
+			});
+		}
+	};
